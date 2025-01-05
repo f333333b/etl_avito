f20849d9e54854c863f9844dbd25b6b4ebc5cfab
f333333b <f33bl3n3ss@gmail.com>
2025-01-05 17:18:37 +0300

    добавлена работа с BeautifulSoup

--- avito_description_fix.py
+++ avito_description_fix.py
@@ -1,6 +1,6 @@
 import re
-import logging
-#from bs4 import BeautifulSoup
+from bs4 import BeautifulSoup as bs
+import os
 import pandas as pd
 (pd.set_option('display.max_columns', 3))
 
@@ -11,8 +11,26 @@ def process_row(row, search_words, keywords, text_sample, end_text):
     vehicle_type = row['VehicleType']
     match_tech = re.search(search_words, description, re.IGNORECASE)  # поиск тех.характеристик
     tech_text = get_tech(match_tech, description)
-    keywords_text = keywords.get(vehicle_type, '')
-    result = f'<b>{title}</b><br />{text_sample}{tech_text}{end_text}{keywords_text}<br />'
+    soup = bs('', 'html.parser')
+    bold_tag = soup.new_tag('b')
+    bold_tag.string = title
+    soup.append(bold_tag)
+    br_tag = soup.new_tag('br')  # Создание тега <br />
+    soup.append(br_tag)
+    soup.append(bs(text_sample, 'html.parser'))
+    if tech_text:
+        p_tag = soup.new_tag('p')
+        strong_tag = soup.new_tag('strong')
+        strong_tag.string = "Характеристики:"
+        p_tag.append(strong_tag)
+        tech_text_soup = bs(tech_text, 'html.parser')
+        p_tag.append(tech_text_soup)
+        soup.append(p_tag)
+    soup.append(bs(end_text, 'html.parser'))
+    keywords_tag = soup.new_tag('p')
+    keywords_tag.string = keywords.get(vehicle_type, '')
+    soup.append(keywords_tag)
+    result = str(soup)
     result_list.append(result)
 
 # функция обработки технических характеристик
@@ -30,7 +48,8 @@ def get_tech(match, description):
             if value.upper() in tech_result:
                 duplicates_removed_temp += 1
                 tech_result = tech_result.replace(value.upper(), '')
-        return f'<p><strong>Характеристики:</strong></p>{tech_result}'
+                print(tech_result)
+        return tech_result
     return ''
 
 # словарь для ключ-слов
@@ -57,30 +76,20 @@ search_words = r"ТЕХНИЧЕСКИЕ ХАРАКТЕРИСТИКИ|ТEXНИЧE
 # основной код
 input_file = input()
 output_file = 'Copy of ' + input_file
-table = pd.read_excel(input_file)
+counter = 0
+while os.path.isfile(output_file):
+    counter += 1
+    output_file = f"Copy of {os.path.splitext(input_file)[0]}_{counter}{os.path.splitext(input_file)[1]}"
+
+table = pd.read_excel(input_file, nrows=5)
 table.to_excel(output_file, engine='openpyxl', index=False)
 result_list = []
 table.apply(process_row, axis=1, args=(search_words, keywords, text_sample, end_text))
 result_dict = {'Description': result_list}
 result_df = pd.DataFrame(result_dict)
-# # логирование
-# logging.basicConfig(
-#     level=logging.INFO,
-#     format="%(asctime)s - %(levelname)s - %(message)s",
-#     handlers=[
-#         logging.FileHandler("avito_description_fix_log.log"),  # Логи пишутся в файл
-#         logging.StreamHandler()  # Логи выводятся в консоль
-#     ]
-# )
-# tech_found = 0
-# duplicates_removed = 0
-# logging.info(f"Общее количество объявлений: {ids_amount}")
-# logging.info(f"Обработано объявлений с характеристиками: {tech_found}")
-# logging.info(f"Удалено дублирующих ключевых слов: {duplicates_removed}")
 
 # запись данных в копию таблицы
 with pd.ExcelWriter(output_file, engine='openpyxl', mode='a', if_sheet_exists='replace') as writer:
     existing_df = pd.read_excel(output_file)
     existing_df['Description'] = result_df['Description']
     existing_df.to_excel(writer, index=False)
-
