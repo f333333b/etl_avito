55776279c2f8f35c63ca903836d845ce724412f7
f333333b <f33bl3n3ss@gmail.com>
2025-03-12 17:38:52 +0300

    added freezing of the first row

--- avito_short_videos.py
+++ avito_short_videos.py
@@ -1,6 +1,8 @@
 # скрипт для вставки в таблицу ссылок на короткие видео из другой таблицы по ID объявления
 
 import pandas as pd
+from openpyxl import load_workbook
+
 
 old_file = pd.read_excel('file_with_short_videos.xlsx')
 new_file = pd.read_excel('file_new.xlsx')
@@ -8,3 +10,9 @@ merged_file = new_file.merge(old_file[['AvitoId', 'VideoFileURL']], on='AvitoId'
 merged_file.to_excel('file_updated.xlsx', index=False)
 added_links_count = merged_file['VideoFileURL'].notna().sum()
 print(f"Добавлено ссылок на видео: {added_links_count}")
+
+# прикрепление первой строки в таблице
+wb = load_workbook("file_updated.xlsx")
+ws = wb.active
+ws.freeze_panes = "A2"
+wb.save("file_updated.xlsx")
