833f33f282f7a4b0e55a853c023b8f76a29f8438
f333333b <f33bl3n3ss@gmail.com>
2025-03-28 19:32:52 +0300

    small fixes

--- avito_table_to_db.py
+++ avito_table_to_db.py
@@ -1,10 +1,13 @@
 import pandas as pd
 from sqlalchemy import create_engine
+from datetime  import datetime
 
-df = pd.read_excel('xm.xlsx')
+df = pd.read_excel(r'C:\Users\fgkh\Desktop\file_updated.xlsx')
 print(df.head())
 print(f"Всего строк: {len(df)}")
 
 engine = create_engine('postgresql://postgres:1@localhost:5432/postgres')
-df.to_sql('xm', engine, if_exists='replace', index=False)
+current_date_and_time = datetime.now().strftime("%Y-%m-%d_%H:%M:%S")
+print(current_date_and_time)
+df.to_sql(f'xm_{current_date_and_time}', engine, if_exists='replace', index=False)
 print("Данные загружены в PostgreSQL")
