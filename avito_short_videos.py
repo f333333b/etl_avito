eb7b3f60902f49812490a186541ade3eb43d0872
f333333b <f33bl3n3ss@gmail.com>
2025-04-05 14:23:08 +0300

    updated

--- avito_short_videos.py
+++ avito_short_videos.py
@@ -4,17 +4,17 @@ import pandas as pd
 from openpyxl import load_workbook
 
 
-old_file = pd.read_excel('file_with_short_videos.xlsx')
-new_file = pd.read_excel(r'C:\Users\fgkh\Desktop\table.xlsx')
+old_file = pd.read_excel(r'C:\Users\user\Desktop\short_videos.xlsx')
+new_file = pd.read_excel(r'C:\Users\user\Desktop\table28.03.2025source.xlsx')
 if 'VideoFileURL' in new_file.columns:
     del new_file['VideoFileURL']
 merged_file = new_file.merge(old_file[['AvitoId', 'VideoFileURL']], on='AvitoId', how='left')
-merged_file.to_excel(r'C:\Users\fgkh\Desktop\file_updated.xlsx', index=False)
+merged_file.to_excel(r'C:\Users\user\Desktop\file_updated.xlsx', index=False)
 added_links_count = merged_file['VideoFileURL'].notna().sum()
 print(f"Добавлено ссылок на видео: {added_links_count}")
 
 # прикрепление первой строки в таблице
-wb = load_workbook("file_updated.xlsx")
+wb = load_workbook(r'C:\Users\user\Desktop\file_updated.xlsx')
 ws = wb.active
 ws.freeze_panes = "A2"
-wb.save("file_updated.xlsx")
+wb.save(r'C:\Users\user\Desktop\file_updated.xlsx')
