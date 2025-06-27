2982bb72a19a2eae528ecdbf56938d09a8df2dd6
f333333b <f33bl3n3ss@gmail.com>
2025-06-27 17:55:55 +0300

    mvp airflow

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,12 @@
 FROM apache/airflow:2.8.1
 
+USER root
+
+RUN mkdir -p /opt/airflow/etl/data \
+    && chown -R airflow: /opt/airflow/etl/data
+
+USER airflow
+
 COPY requirements.txt .
 
 RUN pip install --no-cache-dir -r requirements.txt
--- dags/etl_avito_dag.py
+++ dags/etl_avito_dag.py
@@ -1,26 +1,32 @@
-from datetime import datetime
+import logging
+from datetime import datetime, timedelta
 
 from airflow import DAG
 from airflow.operators.python import PythonOperator
 
-from etl.config import PATH_VARS, REQUIRED_ENV_VARS, load_config
-from etl.main import main as run_etl_pipeline
+from etl.main import run_etl
 
 
 def etl_wrapper():
-    config = load_config(REQUIRED_ENV_VARS, PATH_VARS)
-    run_etl_pipeline(**config)
+    logging.info("Starting ETL pipeline...")
+    run_etl()
 
+
+default_args = {
+    "retries": 2,
+    "retry_delay": timedelta(minutes=5),
+}
 
 with DAG(
     dag_id="etl_avito_dag",
     start_date=datetime(2024, 1, 1),
     schedule="@daily",
     catchup=False,
+    default_args=default_args,
     tags=["avito", "etl"],
     description="DAG для ежедневного запуска ETL пайплайна Avito",
 ) as dag:
 
-    run_pipeline = PythonOperator(task_id="run_etl_pipeline", python_callable=run_etl_pipeline)
+    run_pipeline = PythonOperator(task_id="run_avito_etl_pipeline", python_callable=etl_wrapper)
 
     run_pipeline
